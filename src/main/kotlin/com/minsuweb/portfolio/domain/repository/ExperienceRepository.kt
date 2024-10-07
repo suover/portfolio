02d6656384dc36d959e25edacf368a4629638c41
Min Su Kim <gts2044@gmail.com>
2024-10-07 17:55:20 +0900

    리포지토리 성능 개선

--- src/main/kotlin/com/minsuweb/portfolio/domain/repository/ExperienceRepository.kt
+++ src/main/kotlin/com/minsuweb/portfolio/domain/repository/ExperienceRepository.kt
@@ -2,11 +2,15 @@ package com.minsuweb.portfolio.domain.repository
 
 import com.minsuweb.portfolio.domain.entity.Experience
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface ExperienceRepository:JpaRepository<Experience, Long> {
 
+    @Query("select e from Experience e left join fetch e.details where e.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Experience>
+
+    @Query("select e from Experience e left join fetch e.details where e.id = :id")
     override fun findById(id: Long): Optional<Experience>
 
 }
