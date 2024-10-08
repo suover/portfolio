35800f3ed28f49a8744b21364871112c1c135806
Min Su Kim <gts2044@gmail.com>
2024-10-08 17:40:33 +0900

    리포지토리 개발

--- src/main/kotlin/com/minsuweb/portfolio/presentation/repository/PresentationRepository.kt
+++ src/main/kotlin/com/minsuweb/portfolio/presentation/repository/PresentationRepository.kt
@@ -1,6 +1,36 @@
 package com.minsuweb.portfolio.presentation.repository
 
+import com.minsuweb.portfolio.domain.entity.*
+import com.minsuweb.portfolio.domain.repository.*
 import org.springframework.stereotype.Repository
 
 @Repository
-class PresentationRepository
+class PresentationRepository(
+        private val achievementRepository: AchievementRepository,
+        private val experienceRepository: ExperienceRepository,
+        private val introductionRepository: IntroductionRepository,
+        private val linkRepository: LinkRepository,
+        private val projectRepository: ProjectRepository,
+        private val skillRepository: SkillRepository,
+) {
+
+    fun getActiveAchievements(): List<Achievement> {
+        return achievementRepository.findAllByIsActive(true)
+    }
+    fun getActiveExperiences(): List<Experience> {
+        return experienceRepository.findAllByIsActive(true)
+    }
+    fun getActiveIntroductions(): List<Introduction> {
+        return introductionRepository.findAllByIsActive(true)
+    }
+    fun getActiveLinks(): List<Link> {
+        return linkRepository.findAllByIsActive(true)
+    }
+    fun getActiveProjects(): List<Project> {
+        return projectRepository.findAllByIsActive(true)
+    }
+    fun getActiveSkills(): List<Skill> {
+        return skillRepository.findAllByIsActive(true)
+    }
+
+}
