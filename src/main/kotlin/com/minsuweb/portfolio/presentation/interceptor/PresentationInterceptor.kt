eb1b531b42f531fa30ff297aab634c385231f079
Min Su Kim <gts2044@gmail.com>
2024-10-09 15:06:25 +0900

    인터셉터 개발

--- src/main/kotlin/com/minsuweb/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/minsuweb/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,6 +1,19 @@
 package com.minsuweb.portfolio.presentation.interceptor
 
+import com.minsuweb.portfolio.domain.entity.HttpInterface
+import com.minsuweb.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class PresentationInterceptor
+class PresentationInterceptor(
+        private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+
+    override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
+}
