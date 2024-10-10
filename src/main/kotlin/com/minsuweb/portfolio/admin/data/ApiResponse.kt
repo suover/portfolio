6d602a5a02384b090bc5228e0e1a9548e3688d68
Min Su Kim <gts2044@gmail.com>
2024-10-10 14:13:10 +0900

    공통 개발 - DTO

--- src/main/kotlin/com/minsuweb/portfolio/admin/data/ApiResponse.kt
+++ src/main/kotlin/com/minsuweb/portfolio/admin/data/ApiResponse.kt
@@ -3,4 +3,19 @@ package com.minsuweb.portfolio.admin.data
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
-class ApiResponse<T>(status: HttpStatus) : ResponseEntity<T>(status)
+class ApiResponse<T>(status: HttpStatus) : ResponseEntity<T>(status) {
+
+    companion object {
+        fun successCreate(): ResponseEntity<Any> {
+            return ok("데이터가 저장되었습니다.")
+        }
+
+        fun successUpdate(): ResponseEntity<Any> {
+            return ok("데이터가 수정되었습니다.")
+        }
+
+        fun successDelete(): ResponseEntity<Any> {
+            return ok("데이터가 삭제되었습니다.")
+        }
+    }
+}
