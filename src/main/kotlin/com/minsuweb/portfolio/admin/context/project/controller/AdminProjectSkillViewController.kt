4e20e47e6fe684bf4656e4679a65019a22b1973e
Min Su Kim <gts2044@gmail.com>
2024-10-10 17:25:24 +0900

    조회 개발 - 연관관계 있음

---
+++ src/main/kotlin/com/minsuweb/portfolio/admin/context/project/controller/AdminProjectSkillViewController.kt
@@ -0,0 +1,42 @@
+package com.minsuweb.portfolio.admin.context.project.controller
+
+import com.minsuweb.portfolio.admin.context.project.service.AdminProjectSkillService
+import com.minsuweb.portfolio.admin.data.FormElementDTO
+import com.minsuweb.portfolio.admin.data.SelectFormElementDTO
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/admin/project/skill")
+class AdminProjectSkillViewController(
+        private val adminProjectSkillService: AdminProjectSkillService
+) {
+    @GetMapping
+    fun projectSkill(model: Model): String {
+        val projectList = adminProjectSkillService.getProjectList()
+        val skillList = adminProjectSkillService.getSkillList()
+
+        val formElements = listOf<FormElementDTO>(
+                SelectFormElementDTO("project", 8, projectList),
+                SelectFormElementDTO("skill", 4, skillList)
+        )
+        model.addAttribute("formElements", formElements)
+
+        val table = adminProjectSkillService.getProjectSkillTable()
+        model.addAttribute("table", table)
+        model.addAttribute("detailTable", null)
+
+        val pageAttributes = mutableMapOf<String, Any>(
+                Pair("menuName", "Projects"),
+                Pair("pageName", table.name),
+                Pair("editable", false),
+                Pair("deletable", true),
+                Pair("hasDetails", false),
+        )
+        model.addAllAttributes(pageAttributes)
+
+        return "admin/page-table"
+    }
+}
---
+++ src/main/kotlin/com/minsuweb/portfolio/admin/context/project/service/AdminProjectService.kt
@@ -0,0 +1,30 @@
+package com.minsuweb.portfolio.admin.context.project.service
+
+import com.minsuweb.portfolio.admin.data.TableDTO
+import com.minsuweb.portfolio.admin.exception.AdminBadRequestException
+import com.minsuweb.portfolio.domain.entity.Project
+import com.minsuweb.portfolio.domain.entity.ProjectDetail
+import com.minsuweb.portfolio.domain.repository.ProjectRepository
+import org.springframework.stereotype.Service
+
+@Service
+class AdminProjectService(
+        private val projectRepository: ProjectRepository
+) {
+
+    fun getProjectTable(): TableDTO {
+        val classInfo = Project::class
+        val entities = projectRepository.findAll()
+
+        return TableDTO.from(classInfo, entities, "details", "skills")
+    }
+
+    fun getProjectDetailTable(id: Long?): TableDTO {
+        val classInfo = ProjectDetail::class
+        val entities = if (id != null) projectRepository.findById(id)
+                .orElseThrow { throw AdminBadRequestException("ID ${id}에 해당하는 데이터를 찾을 수 없습니다.") }
+                .details else emptyList()
+
+        return TableDTO.from(classInfo, entities)
+    }
+}
